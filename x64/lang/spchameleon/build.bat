d87d3bd7975a830790d301c0bd7daf9d45ef2066
Sumire <67167154+Wildbrick142@users.noreply.github.com>
2023-03-26 21:30:10 -0400

    chameleon + coquette8

---
+++ x64/lang/spchameleon/build.bat
@@ -0,0 +1,9 @@
+@echo off
+set d=spchameleon
+set txt=global
+set lang=american chinese chinesesimp french german italian japanese korean mexican polish portuguese russian spanish
+for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
+
+gxt2maker %txt% -all dlc.txt
+
+PAUSE
