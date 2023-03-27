05936c71dc6ed716a3a2a1cb983e269d31d9acbb
Sumire <67167154+Wildbrick142@users.noreply.github.com>
2023-03-26 21:38:51 -0400

    trackid p3

---
+++ x64/lang/trackid/community_trackid.bat
@@ -0,0 +1,16 @@
+@echo off
+set d=trackid
+set txt=trackid
+set lang=american chinese chinesesimp french german italian japanese korean mexican polish portuguese russian spanish
+for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
+
+gxt2maker %txt% -all trackid_rsg.txt trackid_rsg_ng.txt trackid_rsg_ng_editor.txt trackid_rsg_dlc.txt trackidvcfm.txt trackidvladivostok.txt trackideurobeat.txt trackidnightcore.txt trackidivports.txt trackidbmrp.txt
+
+PAUSE
+
+
+set txt=global
+for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
+gxt2maker %txt% -all dlc_vladivostok2.txt dlc_vcfm.txt dlc_paradise.txt dlc_ivports.txt dlc_nightcore.txt dlc_eurobeat.txt
+
+PAUSE
---
+++ x64/lang/trackid/standalone_dlc_vcfm.bat
@@ -0,0 +1,16 @@
+@echo off
+set d=trackid
+set txt=trackid
+set lang=american chinese chinesesimp french german italian japanese korean mexican polish portuguese russian spanish
+for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
+
+gxt2maker %txt% -all trackidvcfm.txt
+
+PAUSE
+
+
+set txt=global
+for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
+gxt2maker %txt% -all dlc_vcfm.txt
+
+PAUSE
