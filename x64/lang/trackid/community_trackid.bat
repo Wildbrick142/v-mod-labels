1b247ec998e6d23a9949368b5d59dbda86d139d4
Sumire <67167154+Wildbrick142@users.noreply.github.com>
2023-04-07 07:49:05 -0400

    update p4

--- x64/lang/trackid/community_trackid.bat
+++ x64/lang/trackid/community_trackid.bat
@@ -4,13 +4,13 @@ set txt=trackid
 set lang=american chinese chinesesimp french german italian japanese korean mexican polish portuguese russian spanish
 for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
 
-gxt2maker %txt% -all trackid_rsg.txt trackid_rsg_ng.txt trackid_rsg_ng_editor.txt trackid_rsg_dlc.txt trackidvcfm.txt trackidvladivostok.txt trackideurobeat.txt trackidnightcore.txt trackidivports.txt trackidbmrp.txt
+gxt2maker %txt% -all trackid_rsg.txt trackid_rsg_ng.txt trackid_rsg_ng_editor.txt trackid_rsg_dlc.txt trackidvcfm.txt trackidvladivostok.txt trackideurobeat.txt trackidnightcore.txt trackidivports.txt trackidbmrp.txt trackid3dera.txt
 
 PAUSE
 
 
 set txt=global
 for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\%txt%.gxt2
-gxt2maker %txt% -all dlc_vladivostok2.txt dlc_vcfm.txt dlc_paradise.txt dlc_ivports.txt dlc_nightcore.txt dlc_eurobeat.txt
+gxt2maker %txt% -all dlc_vladivostok2.txt dlc_vcfm.txt dlc_paradise.txt dlc_ivports.txt dlc_nightcore.txt dlc_eurobeat.txt dlc_3dera.txt
 
 PAUSE
