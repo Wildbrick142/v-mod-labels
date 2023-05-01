6a97cf209893f8c0a4e85b1a21694394e9dcdb49
Sumire <67167154+Wildbrick142@users.noreply.github.com>
2023-05-01 10:35:48 +0100

    yet another major tool update p2

--- x64/lang/trackid/standalone_dlc_3dera.bat
+++ x64/lang/trackid/standalone_dlc_3dera.bat
@@ -3,6 +3,6 @@ set d=trackid
 set lang=american chinese chinesesimp french german italian japanese korean mexican polish portuguese russian spanish
 for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\*.gxt2
 
-gxt2maker %txt% -all dlc_3dera.txt trackid3dera.txt
+gxt2maker -all -m 2 dlc_3dera.txt trackid3dera.txt -pack
 
 PAUSE
--- x64/lang/trackid/standalone_dlc_vcfm.bat
+++ x64/lang/trackid/standalone_dlc_vcfm.bat
@@ -3,6 +3,6 @@ set d=trackid
 set lang=american chinese chinesesimp french german italian japanese korean mexican polish portuguese russian spanish
 for %%a in (%lang%) do del /F X:\Users\Sumire\gtamods\x64\lang\%d%\%%a\*.gxt2
 
-gxt2maker -all dlc_vcfm.txt trackidvcfm.txt
+gxt2maker -all -m 2 dlc_vcfm.txt trackidvcfm.txt -pack
 
 PAUSE
